142a77141c9cb05acb4de88aa3d847235d48e04d
kevin_zhu <kevin_zhu@aviva-cofco.com.cn>
2022-06-24 17:59:28 +0800

    charter8 many2many and many2one

---
+++ customAddon/estate/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'estate',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'description': 'desc',
+    'depends' : ['base'],
+    'installable': True,
+    'application': True,
+    'data': [
+            'security/ir.model.access.csv',
+            'views/estate_property_views.xml',
+            'views/estate_property_type_views.xml',
+            'views/estate_property_tag_views.xml',
+            'views/estate_menus.xml',
+        ]
+}
