bb19baf329d7def1d148141b8ac6bbcfe409de93
jameschuh <3201920+jameschuh@users.noreply.github.com>
2022-06-29 10:38:51 +0800

    chapter 9

--- customAddon/estate/models/estate_property.py
+++ customAddon/estate/models/estate_property.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -36,8 +36,35 @@ class EstateProperty(models.Model):
     partner_id = fields.Many2one("res.users", string="Salesperson", index=True, tracking=True, default=lambda self: self.env.user)
 
     # offer_ids
-    offer_ids = fields.One2many('estate.property.offer', 'partner_id', string='Offer')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offer')
 
-    #_sql_constraints = [
-    #    ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
-    #]
+    # computed field area
+    total_area = fields.Float(compute="_compute_total_area", string="Total Area")
+
+    # conputed field area
+    #best_price = fields.Float(string="Best Offer")
+    best_price = fields.Float(compute="_compute_best_offer_price", string="Best Offer", default=0)
+
+    @api.depends("garden_area","living_area")
+    def _compute_total_area(self):
+        for rec in self:
+            rec.total_area = rec.garden_area + rec.living_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_offer_price(self):
+        for rec in self:
+            if rec.offer_ids:
+                for offer in rec.offer_ids:
+                    if rec.best_price:
+                        rec.best_price = offer.price if rec.best_price < offer.price else rec.best_price
+            else:
+                rec.best_price = 0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ''
--- customAddon/estate/models/estate_property_offer.py
+++ customAddon/estate/models/estate_property_offer.py
@@ -1,19 +1,38 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
+import datetime
 
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "estate property offer description"
     _order = "price"
-
+    
     price = fields.Float(string='Price')
     status = fields.Selection([('Accepted','Accepted'),('Refused','Refused')], string='Status')
     partner_id = fields.Many2one('res.partner', required=True, string='Partner')
+        
+
     property_id = fields.Many2one('estate.property', required=True, string='Property')
 
-    #_sql_constraints = [
-    #    ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
-    #]
+    # inverse
+    validity = fields.Integer('Validity(days)', default=7)
+    date_deadline = fields.Date('Deadline', compute="_compute_deadline", inverse="_inverse_validity_from_deadline")
+
+
+    # compute field
+    @api.depends("validity","create_date")
+    def _compute_deadline(self):
+        for rec in self:
+            if rec.validity:
+                d = datetime.timedelta(days=rec.validity)
+                if rec.create_date:
+                    rec.date_deadline = rec.create_date + d
+
+    def _inverse_validity_from_deadline(self):
+        for rec in self:
+            if rec.date_deadline and rec.create_date:
+                rec.validity = (rec.date_deadline - rec.create_date.date()).days
+
